0a89c3ea2587906da2cfcefcdecbc4f28823afd7
uranussg <uranussg@gmail.com>
2020-10-15 15:51:50 -0400

    todo model

--- models/Todo.js
+++ models/Todo.js
@@ -1,2 +1,17 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
+const TodoSchema = new Schema({
+    user: {
+        type: mongoose.isValidObjectId,
+        required: true,
+    },
+    title: {
+        type: String
+    },
+    body: {
+        type: String
+    }
+}, {timestamps:true})
+
+module.exports = mongoose.model('Todo', TodoSchema)
